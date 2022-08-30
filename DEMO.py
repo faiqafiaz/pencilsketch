d416e3dc459f215fb710f3c36aee41ae6920747d
faiqafiaz <108574920+faiqafiaz@users.noreply.github.com>
2022-08-31 01:14:28 +0500

    Update DEMO.py

--- DEMO.py
+++ DEMO.py
@@ -1,5 +1,7 @@
 import streamlit as st
-video_file = open('myvedio.mp4', 'rb')
-video_bytes = video_file.read()
-
-st.video(vedio_bytes)
+def app():
+    st.title('Data Stats')
+    video_file = open('myvedio.mp4', 'rb')
+    video_bytes = video_file.read()
+    vedio=st.video(vedio_bytes)
+    return(vedio)
