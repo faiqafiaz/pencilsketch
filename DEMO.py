485069e3947fb3fd4045df26acbade1f78288e20
faiqafiaz <108574920+faiqafiaz@users.noreply.github.com>
2022-08-31 02:22:52 +0500

    Update DEMO.py

--- DEMO.py
+++ DEMO.py
@@ -3,5 +3,4 @@ def app():
     st.title('Demo')
     video_file = open('myvedio.mp4', 'rb')
     video_bytes = video_file.read()
-    vedio=st.video(video_bytes)
-    return(vedio)
+    st.video(video_bytes)
