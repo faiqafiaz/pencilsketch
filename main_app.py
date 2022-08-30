8f53f3f564708b77f3a1a3994c0ddb31de067006
faiqafiaz <108574920+faiqafiaz@users.noreply.github.com>
2022-08-31 02:49:09 +0500

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -7,6 +7,7 @@ app = MultiApp()
 # Add all your application here
 app.add_app("Home", image_to_sketch.app)
 app.add_app("Demo", DEMO.app)
+st.write("This Web App is to help convert your images to realistic Pencil Sketches")
 
 # The main app
 app.run()
